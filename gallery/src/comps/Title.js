6f4b34f1b3e15e8adcdbdedaa3848e7cdc3e303c
maambo <78373393+maambo@users.noreply.github.com>
2021-03-03 18:22:59 +0200

    Add files via upload

---
+++ gallery/src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>maambo</h1>
+      <h2>My Pictures</h2>
+      <p>The world i live in is a full of suprises.</p>
+    </div>
+  )
+}
+
+export default Title;
